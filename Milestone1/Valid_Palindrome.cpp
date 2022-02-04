3353ef963aae5b72f426a494dbaa71bc71468549
SaloniGhole <99007164+SaloniGhole@users.noreply.github.com>
2022-02-04 13:17:02 +0530

    Create Valid_Palindrome.cpp

---
+++ Milestone1/Valid_Palindrome.cpp
@@ -0,0 +1,18 @@
+bool isPalindrome(string s) {
+        if(s.size() <= 1)
+            return true;
+        int a=0;
+        int b=s.size()-1;
+        while(a < b)
+        {
+        while(a<b && !isalnum(s[a]))
+            a++;
+        while(a<b && !isalnum(s[b]))
+            b--;
+        if(tolower(s[a]) != tolower(s[b]))
+            return false;
+        a++;
+        b--;
+        }
+        return true;
+    }
