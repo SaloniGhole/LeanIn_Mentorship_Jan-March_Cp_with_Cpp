155067eaf528350d37b0f0acea070dcc53ceb82d
SaloniGhole <99007164+SaloniGhole@users.noreply.github.com>
2022-02-27 14:32:36 +0530

    Create Remove_Duplicates_from_Sorted_List.cpp

---
+++ Milestone3/Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,17 @@
+ ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* p = head;
+        while(p != NULL && p->next != NULL)
+        {
+            if(p->val == p->next->val)
+            {
+                p->next = p->next->next;
+            }
+            else
+            
+                {
+                    p=p->next;
+                }
+                
+        }
+        return head;
+    }
